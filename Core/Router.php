7438eb96bea1526a85094b17f2208d1c81bc88a3
nahidul77 <nahidul.mee@gmail.com>
2022-03-23 00:35:54 +0600

    add regular expression to match function to extract controller and action name

--- Core/Router.php
+++ Core/Router.php
@@ -45,11 +45,27 @@ class Router
      */
     public function match($url)
     {
-        foreach($this->routes as $route => $params){
-            if($url == $route){
-                $this->params = $params;
-                return true;
+        // foreach($this->routes as $route => $params){
+        //     if($url == $route){
+        //         $this->params = $params;
+        //         return true;
+        //     }
+        // }
+
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+        
+        if(preg_match($reg_exp, $url, $matches)){
+            //get named capture group values
+            $param = [];
+
+            foreach($matches as $key => $match){
+                if(is_string($key)){
+                    $params[$key] = $match;
+                }
             }
+
+            $this->params = $params;
+            return true;
         }
 
         return false;
