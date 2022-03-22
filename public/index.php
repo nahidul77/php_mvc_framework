e08ed8c0cf3ef71c9a13e55893aeca9bf74a6cb4
nahidul77 <nahidul.mee@gmail.com>
2022-03-22 21:42:33 +0600

    create Router class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php 
+/**
+ * Front Controller
+ */
+// echo 'requested url : '.$_SERVER['QUERY_STRING'];
 
-echo 'requested url : '.$_SERVER['QUERY_STRING'];
+/**
+ * Routing
+ */
+
+ require '../Core/Router.php';
+
+ $router = new Router();
+
+ echo get_class($router);
